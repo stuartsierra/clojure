c922aa7126ee49e0144d6aad5e3386be2c259294
Stuart Sierra <mail@stuartsierra.com>
2010-12-27 09:42:38 -0500

    Add scope.clj

---
+++ src/clj/clojure/scope.clj
@@ -0,0 +1,97 @@
+;;; Resource scopes for Clojure
+
+;; by Stuart Sierra, http://stuartsierra.com/
+;;
+;; References:
+;;
+;; Resource Scopes (Clojure)
+;; http://dev.clojure.org/display/design/Resource+Scopes
+;;
+;; Scope implementation in "streams" branch (Clojure)
+;; https://github.com/clojure/clojure/blob/streams/src/clj/clojure/core.clj#L2005)
+;;
+;; Scope Guard Statement (D language)
+;; http://www.digitalmars.com/d/2.0/statement.html#ScopeGuardStatement
+
+
+(ns clojure.scope)
+
+(defmacro declare-scope
+  "Declares a Var representing a resource scope."
+  [name]
+  {:pre [(symbol? name)]}
+  `(declare ~(vary-meta name assoc :dynamic true)))
+
+(defn handle-scope-exit [s cause]
+  (doseq [[condition f] s]
+    (when (or (= :exit condition)
+	      (= cause condition))
+      (f))))
+
+(defmacro with-scope
+  "Executes body in scope s, which must have been previously declared
+  with declare-scope. When body completes, normally or abnormally,
+  executes callbacks declared with on-exit, on-failure, or
+  on-success. Callbacks will be executed in the reverse order in which
+  they were declared. If a callback function throws an exception,
+  remaining callbacks will not be executed."
+  [s & body]
+  {:pre [(symbol? s)]}
+  `(binding [~s (list)]
+     (try (let [result# (do ~@body)]
+	    (handle-scope-exit ~s :success)
+	    result#)
+	  (catch Throwable t#
+	    (handle-scope-exit ~s :failure)
+	    (throw t#)))))
+
+(defmacro on-exit
+  "Adds a callback to scope s which will execute body when the scope
+  exits, either normally or because of an exception."
+  [s & body]
+  {:pre [(symbol? s)]}
+  `(io! "Attempted to call on-exit in a transaction"
+	(set! ~s (conj ~s [:exit (fn [] ~@body)]))))
+
+(defmacro on-failure
+  "Adds a callback to scope s which will execute body when the scope
+  exits because of a thrown exception."
+  [s & body]
+  {:pre [(symbol? s)]}
+  `(io! "Attempted to call on-failure in a transaction"
+	(set! ~s (conj ~s [:failure (fn [] ~@body)]))))
+
+(defmacro on-success
+  "Adds a callback to scope s which will execute body when the scope
+  exits normally."
+  [s & body]
+  {:pre [(symbol? s)]}
+  `(io! "Attempted to call on-success in a transaction"
+	(set! ~s (conj ~s [:success (fn [] ~@body)]))))
+
+
+;;; Examples:
+(comment
+  (declare-scope *s*)
+
+  (assert (= "12345"
+	     (with-out-str
+	       (with-scope *s* 
+		 (on-success *s* (pr 5)) 
+		 (on-exit *s* (pr 4))
+		 (pr 1)
+		 (on-success *s* (pr 3))
+		 (pr 2)))))
+
+  (assert (= "124"
+	     (with-out-str
+	       (try
+		 (with-scope *s* 
+		   (on-success *s* (pr 5)) 
+		   (on-exit *s* (pr 4)) 
+		   (pr 1)
+		   (pr 2)
+		   (throw (Exception. "Boom!"))
+		   (pr 3)
+		   (on-exit *s* (pr 6)))
+		 (catch Exception e nil))))))
